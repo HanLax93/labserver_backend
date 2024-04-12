cddfd86a1ec9f6fd4ee1d95524ad97f763a98ea7
hanlax93 <hanlax58@gmail.com>
2024-04-12 22:26:34 +0800

    add: remove container

--- src/main/java/site/handglove/labserver/service/ContainerService.java
+++ src/main/java/site/handglove/labserver/service/ContainerService.java
@@ -5,5 +5,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import site.handglove.labserver.model.Container;
 
 public interface ContainerService extends IService<Container> {
-
+    public boolean removeContainer(String name) throws Exception;
 }
--- src/main/java/site/handglove/labserver/service/impl/ContainerServiceImpl.java
+++ src/main/java/site/handglove/labserver/service/impl/ContainerServiceImpl.java
@@ -2,13 +2,26 @@ package site.handglove.labserver.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.dockerjava.api.exception.DockerException;
 
 import site.handglove.labserver.mapper.ContainerMapper;
 import site.handglove.labserver.model.Container;
 import site.handglove.labserver.service.ContainerService;
+import site.handglove.labserver.utils.Helper;
 
 @Service
 public class ContainerServiceImpl extends ServiceImpl<ContainerMapper, Container> implements ContainerService{
-
+    public boolean removeContainer(String name) throws Exception {
+        LambdaQueryWrapper<Container> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Container::getName, name);
+        var container = this.getOne(queryWrapper);
+        try {
+            Helper.removeContainer(container.getStuIndex(), name);
+            return true;
+        } catch (DockerException ex) {
+            throw ex;
+        }
+    }
 }

4a4f8e95731c7717d3c4c3c430f81ff8f9468706
hanlax93 <hanlax58@gmail.com>
2024-04-12 22:25:17 +0800

    add: add field id

--- src/main/java/site/handglove/labserver/model/UserMenu.java
+++ src/main/java/site/handglove/labserver/model/UserMenu.java
@@ -1,6 +1,8 @@
 package site.handglove.labserver.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import lombok.Data;
@@ -8,6 +10,9 @@ import lombok.Data;
 @Data
 @TableName("user_menu")
 public class UserMenu {
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
     @TableField("role_id")
     private Integer roleId;
 
